adf68d271602c8851c3e5c544406bd7aab8bdbbe
ejpon <36930440+ejpon@users.noreply.github.com>
2025-05-08 21:08:39 +0200

    přidány funkce f, U, L

--- main.m
+++ main.m
@@ -11,7 +11,24 @@ clc
 %           lambda ... parametry rozdělení náhodných veličin
 %           x0 ... zadaný testovací počáteční stav
 [N,rp,rt,Nt,alpha,lambda,x0] = carrental_data(2);
-c1= exp(-lambda(1))
-c2= exp(-lambda(2))
-c3= exp(-lambda(3))
-c4= exp(-lambda(4))
+c = exp(-lambda);
+
+f = @f_fce; %je tohle vůbec nutný???
+U = @U_fce;
+L = @L_fce;
+
+
+
+
+
+function y = f_fce(x, u, w)
+    y = max([0;0], min(N, x+[-1;1]*u+[-1 1 0 0; 0 0 -1 1]*w));
+end
+
+function y = U_fce(x)
+    y = -min([x(2), N(1)-x(1), Nt]):min([x(1), N(2)-x(2), Nt]);
+end
+
+function y = L_fce(x,u,w)
+    y = rp*min([x(1)-u, w(1)]) +rp*min([x(2)+u, w(3)]) -rt*abs(u);
+end
